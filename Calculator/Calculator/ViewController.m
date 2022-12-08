522e5f047b098c7ec6c9f7f115d8603d2bf548e9
ajMobileConsulting <ajmobileconsultingsolutions@gmail.com>
2022-12-08 00:38:13 -0500

    Starting to create buttons with constraints. full view coming soon...

--- Calculator/Calculator/ViewController.m
+++ Calculator/Calculator/ViewController.m
@@ -19,13 +19,43 @@ typedef NS_ENUM(NSInteger, Operation) {
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self configureLayoutConstraints];
+    [self createButton:@"9"];
+}
+
+-(void)createButton:(NSString*)string {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom]; // create button
+    [button addTarget:self action:@selector(buttonMethod) forControlEvents:UIControlEventTouchUpInside]; // add action to the button
+    [button setTitle:string forState:UIControlStateNormal]; // assign title to button
+    [self.view addSubview:button]; // add button to subview
+    button.translatesAutoresizingMaskIntoConstraints = NO; // disable autoresizing
+    button.backgroundColor = [UIColor blueColor]; // assign background color
     
-    NSString *answer = [self
-                        preformOperation:OperationAddition
-                        withValue:5 andValue:9];
+    NSLayoutConstraint *ButtonTopConstraint = [button.topAnchor constraintGreaterThanOrEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:100]; // create the topAnchor constraint
+    
+    NSLayoutConstraint *ButtonBottomConstraint = [button.bottomAnchor constraintEqualToAnchor:_operationResult.topAnchor constant:-50]; // create the bottomAnchor constraint
+    
+    NSLayoutConstraint *ButtonTLeadingConstraint = [button.leadingAnchor  constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20]; // create the leadingAnchor constraint
+    
+    NSLayoutConstraint *ButtonWidthConstraint = [ NSLayoutConstraint constraintWithItem:button attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:(self.view.frame.size.width/5)]; // create the widthAnchor constraint
+    NSLog(@"self.view.frame.width - %f", self.view.frame.size.width);
+    NSLayoutConstraint *ButtonHeightConstraint = [ NSLayoutConstraint
+                                                   constraintWithItem:button
+                                                   attribute:NSLayoutAttributeHeight
+                                                   relatedBy:NSLayoutRelationEqual
+                                                   toItem:nil
+                                                   attribute:NSLayoutAttributeNotAnAttribute
+                                                   multiplier:1.0
+                                                   constant:(self.view.frame.size.height/8)]; // create the heightAnchor constraint
     
     
+    NSArray *constraintsArray = @[ ButtonTopConstraint, ButtonBottomConstraint, ButtonTLeadingConstraint, ButtonWidthConstraint, ButtonHeightConstraint ];
     
+    [self.view addConstraints:constraintsArray];
+}
+
+-(void) buttonMethod {
+    NSLog(@"Button Tapped");
 }
 
 - (void)configureLayoutConstraints{
@@ -33,7 +63,11 @@ typedef NS_ENUM(NSInteger, Operation) {
         [UILabel alloc]
         initWithFrame:CGRectMake(0, 0, 200, 44)
     ];
-    _operationResult.text = [NSString stringWithFormat:@"%@",_displayResult];
+    NSString *answer = [self
+                        preformOperation:OperationAddition
+                        withValue:5 andValue:9];
+    
+    _operationResult.text = [NSString stringWithFormat:@"%@",answer];
     _operationResult.translatesAutoresizingMaskIntoConstraints = NO;
     
     [self.view addSubview:_operationResult];
@@ -43,7 +77,8 @@ typedef NS_ENUM(NSInteger, Operation) {
                                                    relatedBy:NSLayoutRelationEqual
                                                    toItem:self.view
                                                    attribute:NSLayoutAttributeCenterX
-                                                   multiplier:1.0 constant:0];
+                                                   multiplier:1.0 constant:0 ];
+    
     NSLayoutConstraint *ConstraintLabelCenterY = [ NSLayoutConstraint
                                                    constraintWithItem:_operationResult
                                                    attribute:NSLayoutAttributeCenterY
@@ -51,7 +86,8 @@ typedef NS_ENUM(NSInteger, Operation) {
                                                    toItem:self.view
                                                    attribute:NSLayoutAttributeCenterY
                                                    multiplier:1.0
-                                                   constant:0];
+                                                   constant:0 ];
+    
     NSArray *constraintsArray = @[ ConstraintLabelCenterY,
                                    ConstraintLabelCenterX ];
     [self.view addConstraints:constraintsArray];
--- Calculator/Calculator/main.m
+++ Calculator/Calculator/main.m
@@ -7,18 +7,13 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
-#import "Aircraft.h"
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
-        Aircraft *plane = [[Aircraft alloc] init];
-        plane.speed = 100;
-        NSLog(@"The speed is %d", plane.speed);
-//        UIViewController *vc = [[UIViewController alloc] init];
-//         = @"1000";
+
     }
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
